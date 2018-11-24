b7e0883079c16efc18578de808b6cea48cb34b12
sidewinder133 <31607918+sidewinder133@users.noreply.github.com>
2018-11-24 02:45:09 -0500

    Implemented mini-pregel
    Used cyclic barrier to synchronize vertices
    Used ArrayBlockingQueues to pass messages to neighbouring vertices
    Used a atomic global vote counter to asses halt condition

--- src/main/java/PageRank.java
+++ src/main/java/PageRank.java
@@ -1,18 +1,74 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class PageRank {
-    public PageRank(int size, int[] fromVertices, int[] toVertices) {
+	private int[][] directionMatrix; //adjacency matrix of vertices. Initialized to all zeros. Set to 1 if vertex of row index has outgoing edge to corresponding column index
+	private int size; 	//total number of vertices
+	private CyclicBarrier barrier;	//barrier to synchronize vertex operations
+//	public CyclicBarrier getBarrier() {
+//		return barrier;
+//	}
+	private CyclicBarrier mainBarrier;
 
+	public CyclicBarrier getMainBarrier() {
+		return mainBarrier;
+	}
+
+	private AtomicInteger haltCount = new AtomicInteger(0);		//Keeps a count of total number of vertices halted after each superstep
+	public AtomicInteger getHaltCount() {
+		return haltCount;
+	}
+
+	private ArrayList<ArrayBlockingQueue<Double>> blockingQueueArray = new ArrayList<ArrayBlockingQueue<Double>>();		//Each vertex reads from the corresponding index of the blocking queue array at the start of each superstep
+	private ConcurrentHashMap<Integer, Double> valueMap;
+	
+    public PageRank(int size, int[] fromVertices, int[] toVertices) {
+    	this.size = size;
+    	this.directionMatrix = new int[size][size];
+    	this.barrier = new CyclicBarrier(size);
+    	this.mainBarrier = new CyclicBarrier(size+1);
+    	valueMap = new ConcurrentHashMap<Integer, Double>(size);
+//    	ArrayBlockingQueue<Double>[] blockingQueueArray = new ArrayBlockingQueue[size];	
+    	for(int i=0; i<size; i++)
+    	{
+    		for(int j=0; j<size; j++)
+    		{
+    			this.directionMatrix[i][j] = 0;
+    		}
+    	}
+    	
+    	for(int i=0; i<fromVertices.length; i++)
+    	{
+    		this.directionMatrix[fromVertices[i]][toVertices[i]] = 1;
+    	}
+    	
+    	for(int i=0; i<size; i++)
+    	{
+    		this.blockingQueueArray.add(new ArrayBlockingQueue<Double>(size));
+//    		this.directionMatrix[fromVertices[i]][toVertices[i]] = 1;
+    	}
     }
 
     public void run() throws InterruptedException {
+    	for(int i=0; i<size; i++)
+    	{
+    		PageRankVertex vertex = new PageRankVertex(i, this.directionMatrix[i], this.barrier, this.size, this.haltCount, this.blockingQueueArray, this.valueMap, this.mainBarrier);
+    		new Thread(vertex).start();
+    	}
 
     }
 
     public static void main(String[] args) {
         // Graph has vertices from 0 to `size-1`
         // and edges 1->0, 2->0, 3->0
-        int size = 4;
+        int size = 5;
         int[] from = {1,2,3};
         int[] to = {0,0,0};
 
@@ -23,5 +79,31 @@ public class PageRank {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        
+        
+        try {
+			pr.getMainBarrier().await();
+		} catch (InterruptedException | BrokenBarrierException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+        
+        pr.printValues();
+        
+        
     }
+
+	private void printValues() {
+		// TODO Auto-generated method stub
+		Double valueSum = 0.0;
+		for(int i=0; i<this.size; i++)
+        {
+			valueSum += this.valueMap.get(i);
+        }
+		for(int i=0; i<this.size; i++)
+		{
+			System.out.println(valueMap.get(i)/valueSum);
+		}
+		
+	}
 }
--- src/main/java/PageRankVertex.java
+++ src/main/java/PageRankVertex.java
@@ -1,37 +1,156 @@
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class PageRankVertex implements Vertex<Double, Double> {
     private static final Double damping = 0.85; // the damping ratio, as in the PageRank paper
     private static final Double tolerance = 1e-4; // the tolerance for converge checking
+    
+    private int vertexID;
+    private Double value;
+    private int[] outArray;
+    private CyclicBarrier barrier;
+	private AtomicInteger haltCount;
+	private ArrayList<ArrayBlockingQueue<Double>> blockingQueueArray = new ArrayList<ArrayBlockingQueue<Double>>();
+    private Double previousValue;
+    private int size;
+    private boolean firstStepFlag = true;
+    private int outEdges = 0;
+	private ConcurrentHashMap valueMap;
+	private CyclicBarrier mainBarrier;
 
-    @Override
+    public PageRankVertex(int i, int[] outwardArray, CyclicBarrier barrier, int size, AtomicInteger haltCount, ArrayList<ArrayBlockingQueue<Double>> blockingQueueArray, ConcurrentHashMap valueMap, CyclicBarrier mainBarrier) {
+		// TODO Auto-generated constructor stub
+    	this.value = (double) (1/(double)size);
+    	this.previousValue = this.value;
+    	this.vertexID = i;
+    	this.outArray = outwardArray;
+    	this.barrier = barrier; 	
+    	this.mainBarrier = mainBarrier;
+    	this.haltCount = haltCount;
+    	this.blockingQueueArray = blockingQueueArray;
+    	this.size = size;
+    	this.valueMap = valueMap;
+    	for(int j=0; j<this.outArray.length; j++)
+    	{
+    		if(outArray[j] == 1)
+    		{
+    			this.outEdges += 1;
+    		}
+    	}
+	}
+
+	@Override
     public int getVertexID() {
-        return 0;
+        return this.vertexID;
     }
 
     @Override
     public Double getValue() {
-        return null;
+        return this.value;
     }
 
     @Override
     public void compute(Collection<Double> messages) {
+    	try {
+//    		System.out.println("started compute for vertex "+ vertexID);
+    		if(firstStepFlag == true)
+    		{
+    			for(int i=0; i<outArray.length; i++)
+    			{
+    				if(outArray[i] == 1)
+    				{
+    					sendMessageTo(i, (double)(value/(double)this.outEdges));
+    				}
+    			}
+    			barrier.await();
+    			firstStepFlag = false;
+    		}
+    		else
+    		{
+    			barrier.await();
+    			this.previousValue = this.value;
+    			Double sum = 0.0;
+    			while(!blockingQueueArray.get(this.vertexID).isEmpty())
+    			{
+    				sum += blockingQueueArray.get(this.vertexID).remove();
+    			}
+    			value = (0.15/(double)size) + damping*sum;
+    			barrier.await();
+    			for(int i=0; i<outArray.length; i++)
+    			{
+    				if(outArray[i] == 1)
+    				{
+    					sendMessageTo(i, (double)(value/(double)this.outEdges));
+    				}
+    			}
+    			voteToHalt();
+    			barrier.await();
+//    			System.out.println(haltCount);
+//    			System.out.println(this.vertexID + " - " + this.value + " halt count " + haltCount);
+//    			barrier.await();
+    			
+//    			barrier.await();
+    		}
+    		
+    		
+    		
+//    		System.out.println("started thread for vertex "+ vertexID + " value = " + this.value);
+//    		this.haltCount.incrementAndGet();    	
+					
+//			System.out.println(haltCount);
+			
+    	} catch (InterruptedException | BrokenBarrierException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
     }
 
     @Override
     public void sendMessageTo(int vertexID, Double message) {
-
+    	this.blockingQueueArray.get(vertexID).add(message);
+//    	System.out.println("added to queue in "+ this.vertexID);
     }
 
     @Override
     public void voteToHalt() {
-
+    	if(Math.abs(this.value - this.previousValue) < tolerance)
+    	{
+    		haltCount.incrementAndGet();
+    	}
     }
 
     @Override
     public void run() {
-
+    	
+    	while(haltCount.get() != size)
+    	{
+    		haltCount.set(0);
+    		compute(this.blockingQueueArray.get(this.vertexID));
+    	}
+    	
+    	
+    	valueMap.put(this.vertexID, this.value);
+    	
+    	try {
+			mainBarrier.await();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BrokenBarrierException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
     }
 }
